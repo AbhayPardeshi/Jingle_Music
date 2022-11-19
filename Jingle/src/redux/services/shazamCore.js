6cae91ee14eb86ac2cd1976ae0ef8a74f3e3a7a9
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-11-19 23:39:51 +0530

    security changes

--- Jingle/src/redux/services/shazamCore.js
+++ Jingle/src/redux/services/shazamCore.js
@@ -4,11 +4,9 @@ export const shazamCoreApi = createApi({
   reducerPath: "shazamCore",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+
     prepareHeaders: (headers, { getState }) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        "e6d0f68ce2mshab3d247b24ee397p117587jsn8e531528287b"
-      );
+      headers.set("X-RapidAPI-Key", process.env.REACT_APP_SHAZAM_CORE_API_KEY);
       return headers;
     },
   }),
